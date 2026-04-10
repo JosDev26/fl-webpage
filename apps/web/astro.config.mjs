1c49ece4cb23e2ff27aa0e45c4f7379d14e574d7
JosDev26 <josueherreradev@gmail.com>
2026-04-09 23:21:46 -0600

    Update astro.config.mjs

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,4 +5,5 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  site: process.env.SITE_URL || 'http://localhost:4321',
 });
